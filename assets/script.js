d7f624c7acebf53222912168ab5aac556dcdeeac
GLuna1982 <gluna0705@gmail.com>
2022-05-22 12:37:00 -0400

    added password length feature and validation

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,29 @@
 // Assignment code here
+var generatePassword = function() {
+    
+};
 
+// Get password length and validate if length is good
+var getPLength = function() {
+    var pLength = window.prompt("How long do you want the password to be? (Password can't be no less than 8 characters and no more than 128 characters");
+    
+    //validate if length value is valid if not restart getPLenght function
+    if (pLength < 8 || pLength > 128) {
+        window.alert("Please input a valid answer.")
+        getPLength();
+    }
+    else {
+        console.log(pLength);
+        return pLength;
+    }
+};
+
+// Password Object
+var passwordInfo = {
+    pLength: getPLength(),
+
+
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
